98ac861a1af746c094c298944b784ab73593cf0d
Hemanth0701 <hemanthkumar0701m@gmail.com>
2025-07-04 13:05:06 +0530

    Intial commit

---
+++ src/main/java/com/tenjiku/auth_service/AuthServiceManagementApplication.java
@@ -0,0 +1,13 @@
+package com.tenjiku.auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthServiceManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthServiceManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenjiku/auth_service/AuthServiceManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenjiku.auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
